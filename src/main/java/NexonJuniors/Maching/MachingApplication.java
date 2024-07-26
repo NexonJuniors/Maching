858ac0f3ea76938fcf0d2c90460fcd299ab12794
bainaryho <rkswl233@naver.com>
2024-07-27 05:52:34 +0900

    docs : set dotenv in Application and set security

--- src/main/java/NexonJuniors/Maching/MachingApplication.java
+++ src/main/java/NexonJuniors/Maching/MachingApplication.java
@@ -1,12 +1,20 @@
 package NexonJuniors.Maching;
 
+
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MachingApplication {
-
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(MachingApplication.class, args);
 	}
 
