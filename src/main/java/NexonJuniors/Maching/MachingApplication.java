8e2aa1a78c4d0cedce07dfd1587df2e734f5c715
GrO0vy <lmc2819@naver.com>
2024-07-26 22:00:28 +0900

    initial commit

---
+++ src/main/java/NexonJuniors/Maching/MachingApplication.java
@@ -0,0 +1,13 @@
+package NexonJuniors.Maching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NexonJuniors/Maching/MachingApplicationTests.java
@@ -0,0 +1,13 @@
+package NexonJuniors.Maching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
