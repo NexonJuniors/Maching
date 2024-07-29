17ced7d7fff6e3f7e99cc424d0fc343d260857dc
GrO0vy <lmc2819@naver.com>
2024-07-30 05:45:26 +0900

    fix : 캐릭터 검색 동작을 Rest 형태로 수정

--- src/main/java/NexonJuniors/Maching/controller/HomeController.java
+++ src/main/java/NexonJuniors/Maching/controller/HomeController.java
@@ -1,7 +1,6 @@
 package NexonJuniors.Maching.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 // 기본 URL(/)로 접근했을 때 home.html을 보여주는 컨트롤러
@@ -10,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String home() {
         return "home"; // resources/templates/home.html을 렌더링
     }
+
+    @GetMapping("/info")
+    public String info(){ return "character";}
 }
