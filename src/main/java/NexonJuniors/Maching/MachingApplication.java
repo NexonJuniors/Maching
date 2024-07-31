2885aad83e19a902f3df38cf6a542981949acfb2
GrO0vy <lmc2819@naver.com>
2024-08-01 01:24:15 +0900

    set : 환경 변수 Bean 객체로 등록

--- src/main/java/NexonJuniors/Maching/MachingApplication.java
+++ src/main/java/NexonJuniors/Maching/MachingApplication.java
@@ -2,23 +2,9 @@ package NexonJuniors.Maching;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class MachingApplication {
 	public static void main(String[] args) {
-
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
 		SpringApplication.run(MachingApplication.class, args);
-	}
-
-	@Bean
-	public Dotenv dotenv() {
-		return Dotenv.configure().load();
 	}
 }
---
+++ src/main/java/NexonJuniors/Maching/config/EnvConfig.java
@@ -0,0 +1,17 @@
+package NexonJuniors.Maching.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EnvConfig {
+    @Bean
+    public Dotenv dotenv() {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        return Dotenv.configure().load();
+    }
+}
