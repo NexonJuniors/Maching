f06909967ab2b84a38d137d0569e2e600a404ea0
bainaryho <rkswl233@naver.com>
2024-08-02 01:17:14 +0900

    refactor : clean character js, html

--- src/main/resources/static/character.js
+++ src/main/resources/static/character.js
@@ -3,6 +3,35 @@ const info = JSON.parse(localStorage.getItem("info"))
 const basicInfo = info.basicInfo;
 const statInfo = info.statInfo;
 
+// 전투력은 따로 포맷하여 업데이트
+const powerStat = statInfo.final_stat.find(stat => stat.stat_name === "전투력");
+if (powerStat) {
+    document.getElementById("power").innerText = formatNumber(powerStat.stat_value);
+} else {
+    console.warn('전투력 stat not found');
+}
+
+// 0인값들은 뒤에 글자랑같이 안나오게 해야함, 이후 업데이트
+updateStat("보스 몬스터 데미지", "bossDamage");
+updateStat("방어율 무시", "bangMoo");
+updateStat("아케인포스", "arcaneForce");
+updateStat("어센틱포스", "authenticForce");
+updateStat("버프 지속시간", "buffDuration");
+updateStat("재사용 대기시간 감소 (초)", "cooldownNum");
+updateStat("재사용 대기시간 감소 (%)", "cooldownPer");
+updateStat("재사용 대기시간 미적용", "cooldownNow");
+updateStat("소환수 지속시간 증가", "minions");
+
+
+// 로컬 스토리지에 저장된 정보를 HTML 요소 값으로 저장
+document.getElementById("characterImage").setAttribute("src", basicInfo.character_image)
+document.getElementById("characterName").innerText = basicInfo.character_name
+document.getElementById("characterClass").innerText = basicInfo.character_class
+document.getElementById("worldName").innerText = basicInfo.world_name
+document.getElementById("characterLevel").innerText = basicInfo.character_level
+document.getElementById("characterExp").innerText = basicInfo.character_exp_rate
+document.getElementById("characterGuildName").innerText = basicInfo.character_guild_name
+
 // statInfo에서 특정 stat_name을 찾아서 해당 값을 지정된 elementId에 설정하는 함수
 function updateStat(statName, elementId) {
     const stat = statInfo.final_stat.find(stat => stat.stat_name === statName);
@@ -29,32 +58,3 @@ function formatNumber(number) {
     }
     return result;
 }
-
-// 전투력은 따로 포맷하여 업데이트
-const powerStat = statInfo.final_stat.find(stat => stat.stat_name === "전투력");
-if (powerStat) {
-    document.getElementById("power").innerText = formatNumber(powerStat.stat_value);
-} else {
-    console.warn('전투력 stat not found');
-}
-
-// 0인값들은 뒤에 글자랑같이 안나오게 해야함, 이후 업데이트
-updateStat("보스 몬스터 데미지", "bossDamage");
-updateStat("방어율 무시", "bangMoo");
-updateStat("아케인포스", "arcaneForce");
-updateStat("어센틱포스", "authenticForce");
-updateStat("버프 지속시간", "buffDuration");
-updateStat("재사용 대기시간 감소 (초)", "cooldownNum");
-updateStat("재사용 대기시간 감소 (%)", "cooldownPer");
-updateStat("재사용 대기시간 감소 미적용", "cooldownNow");
-updateStat("소환수 지속시간 증가", "minions");
-
-
-// 로컬 스토리지에 저장된 정보를 HTML 요소 값으로 저장
-document.getElementById("characterImage").setAttribute("src", basicInfo.character_image)
-document.getElementById("characterName").innerText = basicInfo.character_name
-document.getElementById("characterClass").innerText = basicInfo.character_class
-document.getElementById("worldName").innerText = basicInfo.world_name
-document.getElementById("characterLevel").innerText = basicInfo.character_level
-document.getElementById("characterExp").innerText = basicInfo.character_exp_rate
-document.getElementById("characterGuildName").innerText = basicInfo.character_guild_name
