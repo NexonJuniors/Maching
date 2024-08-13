2474bf8280f1bbdbc4f3b7199f53ed2ea9452ff5
GrO0vy <lmc2819@naver.com>
2024-08-13 20:40:44 +0900

    feat : 채팅방 입장 시 캐릭터 기본정보 화면에 표시

--- src/main/resources/static/js/chatting.js
+++ src/main/resources/static/js/chatting.js
@@ -4,6 +4,8 @@ const roomId = localStorage.getItem("roomId")
 const info = JSON.parse(localStorage.getItem("info"))
 const nickname = info.basicInfo.character_name
 
+
+// 메세지를 보낼 때 헤더에 포함시킬 방번호 저장
 const connectHeaders = {'roomId' : `${roomId}`}
 
 stompClient.connect({}, onConnected)
@@ -17,9 +19,8 @@ function receiveMessage(message){
 
         for(let i = 0; i < users.length; i++){
             const user = users[i]
-            const basicInfo = user.basicInfo
+            loadBasic(user, i + 1)
 
-            document.getElementById(`user${i + 1}`).innerText = basicInfo.character_name;
         }
 
         const newChat = document.createElement("div")
@@ -34,4 +35,62 @@ function onConnected(){
     }, connectHeaders)
 
     stompClient.send("/app/enterRoom",connectHeaders, `${nickname}`);
+}
+
+// 길드, 캐릭터 이름, 레벨 등 기본 정보를 화면에 표시
+function loadBasic(user, idx){
+    const basicInfo = user.basicInfo
+    const characterName = basicInfo.character_name
+    const characterImg = basicInfo.character_image
+    const guild = basicInfo.character_guild_name
+    const level = basicInfo.character_level
+    const characterClass = basicInfo.character_class
+
+    const statInfo = user.statInfo
+    const powerStat = statInfo.final_stat.find(stat => stat.stat_name === "전투력").stat_value
+
+    const unionInfo = user.unionInfo
+    const unionLevel = unionInfo.union_level
+
+    const classMainStatInfo = user.classMainStatInfo
+    const mainStatImgSrc = `../static/image/badge/${classMainStatInfo}.png`
+    const classMinutesInfo = user.classMinutesInfo
+
+    document.getElementById(`characterName${idx}`).innerText = characterName
+    document.getElementById(`characterImage${idx}`).setAttribute('src',characterImg)
+    document.getElementById(`characterGuildName${idx}`).innerText = guild
+    document.getElementById(`characterLevel${idx}`).innerText = level
+    document.getElementById(`characterClass${idx}`).innerText = characterClass
+    document.getElementById(`power${idx}`).innerText = formatNumber(powerStat)
+    document.getElementById(`unionLevel${idx}`).innerText = unionLevel
+    document.getElementById(`minutes${idx}`).innerText = `${classMinutesInfo} 분 주기`
+
+    document.getElementById(`tooltip${idx}`).innerText = `내 주스탯은 ${classMainStatInfo}`
+    document.getElementById(`badge${idx}`).setAttribute('src', mainStatImgSrc)
+}
+
+function loadDetails(user, idx){
+
+}
+
+// 이미지 경로를 동적으로 생성하는 함수, 이거 나중에 basePath를 그냥 지정하도록 리펙토링 예정
+function getImagePath(basePath, fileName, extension = 'png') {
+    return `${basePath}${fileName}.${extension}`;
+}
+
+// 숫자를 만, 억 단위로 포맷하는 함수
+function formatNumber(number) {
+    if (number === 0) return '0';
+    const units = ['', '만', '억'];
+    let unitIndex = 0;
+    let result = '';
+    while (number > 0) {
+        const part = number % 10000;
+        if (part > 0) {
+            result = part + units[unitIndex] + result;
+        }
+        number = Math.floor(number / 10000);
+        unitIndex++;
+    }
+    return result;
 }
