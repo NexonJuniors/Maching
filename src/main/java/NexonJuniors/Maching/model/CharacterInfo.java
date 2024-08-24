4df257a01fae154cf47fd6da32726decfcb240e4
GrO0vy <lmc2819@naver.com>
2024-08-24 19:22:53 +0900

    feat: 시드링 정보 저장하는 멤버변수 추가

--- src/main/java/NexonJuniors/Maching/model/CharacterInfo.java
+++ src/main/java/NexonJuniors/Maching/model/CharacterInfo.java
@@ -1,5 +1,10 @@
 package NexonJuniors.Maching.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CharacterInfo {
 
     private BasicInfo basicInfo;
@@ -10,72 +15,7 @@ public class CharacterInfo {
     private String characterClassInfo; // 직업 정보 (ex 비숍)
     private String classMinutesInfo; // 주기 정보 (ex "2")
     private String classMainStatInfo; // 주스탯 정보 (ex "str")
-
-    // Getter 및 Setter
-
-    public CharacterEquipmentInfo getCharacterEquipmentInfo() {
-        return characterEquipmentInfo;
-    }
-
-    public void setCharacterEquipmentInfo(CharacterEquipmentInfo characterEquipmentInfo) {
-        this.characterEquipmentInfo = characterEquipmentInfo;
-    }
-
-    public BasicInfo getBasicInfo() {
-        return basicInfo;
-    }
-
-    public void setBasicInfo(BasicInfo basicInfo) {
-        this.basicInfo = basicInfo;
-    }
-
-    public StatInfo getStatInfo() {
-        return statInfo;
-    }
-
-    public void setStatInfo(StatInfo statInfo) {
-        this.statInfo = statInfo;
-    }
-
-    public String getCharacterClassInfo() {
-        return characterClassInfo;
-    }
-
-    public void setCharacterClassInfo(String characterClassInfo) {
-        this.characterClassInfo = characterClassInfo;
-    }
-
-    public String getClassMinutesInfo() {
-        return classMinutesInfo;
-    }
-
-    public void setClassMinutesInfo(String classMinutesInfo) {
-        this.classMinutesInfo = classMinutesInfo;
-    }
-
-    public String getClassMainStatInfo() {
-        return classMainStatInfo;
-    }
-
-    public void setClassMainStatInfo(String classMainStatInfo) {
-        this.classMainStatInfo = classMainStatInfo;
-    }
-
-    public UnionInfo getUnionInfo() {
-        return unionInfo;
-    }
-
-    public void setUnionInfo(UnionInfo unionInfo) {
-        this.unionInfo = unionInfo;
-    }
-
-    public HexaSkillInfo getHexaSkillInfo() {
-        return hexaSkillInfo;
-    }
-
-    public void setHexaSkillInfo(HexaSkillInfo hexaSkillInfo) {
-        this.hexaSkillInfo = hexaSkillInfo;
-    }
+    private SpecialRingInfo specialRingInfo;
 
     @Override
     public String toString() {
