2f96394bcb03ec053c96a1e8ae5c28b5f5d27374
GrO0vy <lmc2819@naver.com>
2024-09-10 03:34:31 +0900

    feat: 이메일 인증 요청 URL 추가

--- src/main/java/NexonJuniors/Maching/user/UserController.java
+++ src/main/java/NexonJuniors/Maching/user/UserController.java
@@ -1,7 +1,9 @@
 package NexonJuniors.Maching.user;
 
+import NexonJuniors.Maching.user.dto.EmailAuthDto;
 import NexonJuniors.Maching.user.dto.SignUpDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +17,10 @@ public class UserController {
     @PostMapping("/user")
     public void registerUser(@RequestBody SignUpDto signUpDto){
         userService.registerUser(signUpDto);
+    }
+
+    @PostMapping("/emailAuth")
+    public void IssueEmailAuthCode(@RequestBody EmailAuthDto emailAuthDto){
+        userService.IssueEmailAuthCode(emailAuthDto);
     }
 }
