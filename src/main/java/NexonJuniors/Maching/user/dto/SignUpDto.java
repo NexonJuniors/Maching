e2b360ebaefe09904bd2aa697aa799075cf3f7a6
GrO0vy <lmc2819@naver.com>
2024-09-09 23:00:47 +0900

    feat: 회원가입 요청 시 서버로 전달할 DTO 추가

---
+++ src/main/java/NexonJuniors/Maching/user/dto/SignUpDto.java
@@ -0,0 +1,11 @@
+package NexonJuniors.Maching.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignUpDto {
+    private String userId;
+    private String userPw;
+}
