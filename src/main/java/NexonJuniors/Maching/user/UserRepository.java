90055d6bf180dc6017bec923d74f48cb24ce3ad6
GrO0vy <lmc2819@naver.com>
2024-09-10 18:20:31 +0900

    feat: 유저 아이디로 해당 엔티티 찾는 메소드 추가

--- src/main/java/NexonJuniors/Maching/user/UserRepository.java
+++ src/main/java/NexonJuniors/Maching/user/UserRepository.java
@@ -2,6 +2,9 @@ package NexonJuniors.Maching.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Boolean existsByUserId(String userId);
+    Optional<UserEntity> findByUserId(String userId);
 }
