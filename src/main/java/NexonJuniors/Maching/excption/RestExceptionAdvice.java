d55db6f696ab7a468a97ff1ab823cfee5beefdbd
GrO0vy <lmc2819@naver.com>
2024-09-10 15:47:35 +0900

    feat: RestControllerAdvice 에 UserException 예외 처리 핸들러 추가

--- src/main/java/NexonJuniors/Maching/excption/RestExceptionAdvice.java
+++ src/main/java/NexonJuniors/Maching/excption/RestExceptionAdvice.java
@@ -1,5 +1,7 @@
-package NexonJuniors.Maching.excption.api;
+package NexonJuniors.Maching.excption;
 
+import NexonJuniors.Maching.excption.api.ApiException;
+import NexonJuniors.Maching.excption.user.UserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,9 +12,20 @@ import java.util.HashMap;
 // Api 관련 Exception Handler 모아 놓은 클래스
 
 @RestControllerAdvice
-public class ApiExceptionAdvice {
+public class RestExceptionAdvice {
     @ExceptionHandler(ApiException.class)
     public ResponseEntity apiException(ApiException exception){
+        HttpStatus status = exception.getCode().getStatus();
+        String message = exception.getCode().getMessage();
+
+        HashMap<String, String> response = new HashMap<>();
+        response.put("message", message);
+
+        return ResponseEntity.status(status).body(response);
+    }
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity userException(UserException exception){
         HttpStatus status = exception.getCode().getStatus();
         String message = exception.getCode().getMessage();
 
