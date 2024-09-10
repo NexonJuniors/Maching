5050b6738ea6347a3d8ec59d3d93397cacc547d1
GrO0vy <lmc2819@naver.com>
2024-09-10 14:30:15 +0900

    set: 환경 변수 설정

--- src/main/java/NexonJuniors/Maching/config/EnvConfig.java
+++ src/main/java/NexonJuniors/Maching/config/EnvConfig.java
@@ -8,16 +8,9 @@ import org.springframework.context.annotation.Configuration;
 public class EnvConfig {
     @Bean
     public Dotenv dotenv() {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("REDIS_URL", dotenv.get("REDIS_URL"));
-        System.setProperty("REDIS_PORT", dotenv.get("REDIS_PORT"));
-        System.setProperty("REDIS_USERNAME", dotenv.get("REDIS_USERNAME"));
-        System.setProperty("REDIS_PASSWORD", dotenv.get("REDIS_PASSWORD"));
-        System.setProperty("MAIL_ADDRESS", dotenv.get("MAIL_ADDRESS"));
-        System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
-        return Dotenv.configure().load();
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        return dotenv;
     }
 }
