e54be5a3b5cd04ae8d7d699187e504603d8d65f4
GrO0vy <lmc2819@naver.com>
2024-09-10 18:19:52 +0900

    feat: 로그인 요청 시 발생할 수 있는 예외코드 추가

--- src/main/java/NexonJuniors/Maching/excption/user/UserExceptionCode.java
+++ src/main/java/NexonJuniors/Maching/excption/user/UserExceptionCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum UserExceptionCode {
     EXIST_USER(HttpStatus.BAD_REQUEST, "이미 가입된 이메일 입니다."),
-    IS_NOT_VALID_CODE(HttpStatus.BAD_REQUEST, "잘못된 이메일 인증 코드입니다.");
+    IS_NOT_VALID_CODE(HttpStatus.BAD_REQUEST, "잘못된 이메일 인증 코드입니다."),
+    NOT_EXIST_USER(HttpStatus.BAD_REQUEST, "회원정보가 일치하지 않습니다."),
+    IS_NOT_VALID_PASSWORD(HttpStatus.BAD_REQUEST, "회원정보가 일치하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
