b8ee8bdb69ae127c495deb9181e539e5b352cc84
GrO0vy <lmc2819@naver.com>
2024-10-31 18:15:08 +0900

    feat : JWT 검증 유효성 검사, 만료기간 검사 기능 추가

--- src/main/java/NexonJuniors/Maching/utils/JwtUtil.java
+++ src/main/java/NexonJuniors/Maching/utils/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,11 @@ import java.time.Instant;
 import java.util.Date;
 
 @Component
+@Slf4j
 public class JwtUtil {
+    private final static int ACCESS_TOKEN_TIME = 60 * 30;
+    private final static int REFRESH_TOKEN_TIME = 60 * 60 * 24;
+    private final static String REFRESH_TOKEN_ISSUER = "REF_MACHING";
     private final Key signingKey;
     private final JwtParser jwtParser;
 
@@ -25,15 +30,38 @@ public class JwtUtil {
     }
 
     // JWT 만드는 메소드
-    public String generateToken(Long id, Integer time){
+    public String generateToken(Long id, int time){
         Claims claims = Jwts.claims()
                 .setSubject(String.valueOf(id))
                 .setIssuedAt(Date.from(Instant.now()))
                 .setExpiration(Date.from(Instant.now().plusSeconds(time)));
 
+        if(time == REFRESH_TOKEN_TIME) claims.setIssuer(REFRESH_TOKEN_ISSUER);
+
         return Jwts.builder()
                 .setClaims(claims)
                 .signWith(signingKey)
                 .compact();
+    }
+
+    public boolean isValidate(String token){
+        try{
+            parseClaims(token);
+            return true;
+        }
+        catch (Exception e){
+            log.warn("유효하지 않은 토큰");
+            return false;
+        }
+    }
+
+    public boolean isExpired(String token){
+        Claims claims = parseClaims(token);
+
+        return ((Date)claims.getExpiration()).before(new Date());
+    }
+
+    public Claims parseClaims(String token){
+        return jwtParser.parseClaimsJws(token).getBody();
     }
 }
